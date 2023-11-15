d29a50ea3440a9723f73d8c837bf5f7a12b02bef
profile-lab <info@profile-lab.it>
2023-11-15 19:46:04 +0100

    rimosse rotte lcshop install

--- Cms/Config/Routes.php
+++ Cms/Config/Routes.php
@@ -2,12 +2,6 @@
 
 namespace Config;
 
-if (file_exists(APPPATH . 'Routes/lcshop-install.php')) {
-    require APPPATH . 'Routes/lcshop-install.php';
-} else if (file_exists(ROOTPATH . 'LcShop/Cms/Routes/lcshop-install.php')) {
-    require ROOTPATH . 'LcShop/Cms/Routes/lcshop-install.php';
-}
-
 if (file_exists(APPPATH . 'Routes/lcshop-admin.php')) {
     require APPPATH . 'Routes/lcshop-admin.php';
 } else if (file_exists(ROOTPATH . 'LcShop/Cms/Routes/lcshop-admin.php')) {
