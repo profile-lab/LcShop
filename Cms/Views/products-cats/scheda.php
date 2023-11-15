412111bed75deed36bb96fa3e9c5a49173a237f0
profile-lab <info@profile-lab.it>
2023-11-15 22:02:12 +0100

    struttura scheda categorie prodotti

--- Cms/Views/products-cats/scheda.php
+++ Cms/Views/products-cats/scheda.php
@@ -4,6 +4,7 @@
     let count_rows = 0;
 </script>
 <form class="form save_after_proc" method="POST" action="">
+    <?php /*
     <div id="scheda_tools" class="w-100 sticky-top mt-2 bg-white">
         <div class="container-fluid bg-dark text-light p-3 mt-3 mb-4">
             <div class="row d-md-flex justify-content-between align-items-center">
@@ -22,30 +23,51 @@
 
     <h1><?= $module_name ?></h1>
     <?= user_mess($ui_mess, $ui_mess_type) ?>
+   */ ?>
+
+    <?= user_mess($ui_mess, $ui_mess_type) ?>
+    <div class="d-md-flex">
+        <div class="d-flex align-items-center">
+            <div class="titoli_scheda">
+                <?php if ($entity->id) { ?>
+                    <h3><?= ($entity->modello) ? $entity->modello : $entity->nome ?></h3>
+                <?php } else { ?>
+                    <h3>Crea nuova categoria</h3>
+                <?php } ?>
+            </div>
+        </div>
+        <div class="d-flex align-items-center ">
+            <div>
+                <button type="submit" name="save" value="save" class="btn bottone_salva btn-primary"><span class="oi oi-check"></span>Salva</button>
+            </div>
+        </div>
+    </div>
     <div class="row form-row">
         <div class="col-12 col-lg-9 scheda_body">
-            <div class="row">
-                <?= view('Lc5\Cms\Views\form-cmp/text', ['item' => ['label' => 'Nome', 'name' => 'nome', 'value' => $entity->nome, 'width' => 'col-md-12', 'placeholder' => 'Nome']]) ?>
-                <?= view('Lc5\Cms\Views\form-cmp/text', ['item' => ['label' => 'Titolo', 'name' => 'titolo', 'value' => $entity->titolo, 'width' => 'col-md-12', 'placeholder' => 'Titolo']]) ?>
-            </div>
-            <div class="row form-row">
-                <?= view('Lc5\Cms\Views\form-cmp/html-editor', ['item' => ['label' => 'Testo', 'name' => 'testo', 'value' => (isset($entity->testo)) ? $entity->testo : '', 'width' => 'col-md-12', 'placeholder' => '...']]) ?>
-            </div>
+            <div class="first-row">
+                <div class="row">
+                    <?= view('Lc5\Cms\Views\form-cmp/text', ['item' => ['label' => 'Nome', 'name' => 'nome', 'value' => $entity->nome, 'width' => 'col-md-12', 'placeholder' => 'Nome']]) ?>
+                    <?= view('Lc5\Cms\Views\form-cmp/text', ['item' => ['label' => 'Titolo', 'name' => 'titolo', 'value' => $entity->titolo, 'width' => 'col-md-12', 'placeholder' => 'Titolo']]) ?>
+                </div>
+                <div class="row form-row">
+                    <?= view('Lc5\Cms\Views\form-cmp/html-editor', ['item' => ['label' => 'Testo', 'name' => 'testo', 'value' => (isset($entity->testo)) ? $entity->testo : '', 'width' => 'col-md-12', 'placeholder' => '...']]) ?>
+                </div>
 
-            <div class="row form-row">
-                <?= view('Lc5\Cms\Views\form-cmp/img-gallery', ['item' => ['label' => 'Gallery', 'name' => 'gallery', 'value' => (isset($entity->gallery)) ? $entity->gallery : '{}', 'width' => 'col-12', 'gallery_obj' => (isset($entity->gallery_obj)) ? $entity->gallery_obj : []]]) ?>
+                <div class="row form-row">
+                    <?= view('Lc5\Cms\Views\form-cmp/img-gallery', ['item' => ['label' => 'Gallery', 'name' => 'gallery', 'value' => (isset($entity->gallery)) ? $entity->gallery : '{}', 'width' => 'col-12', 'gallery_obj' => (isset($entity->gallery_obj)) ? $entity->gallery_obj : []]]) ?>
+                </div>
             </div>
-
             <?php if ($entity->id) { ?>
-                <div class="row form-row border badge-light mt-4 mx-1 p-3">
-                    <?= view('Lc5\Cms\Views\form-cmp/text', ['item' => ['label' => 'SEO title', 'name' => 'seo_title', 'value' => $entity->seo_title, 'width' => 'col-md-12', 'placeholder' => 'seo title', 'id' => null]]) ?>
-                    <?= view('Lc5\Cms\Views\form-cmp/text-area', ['item' => ['label' => 'SEO description', 'name' => 'seo_description', 'value' => $entity->seo_description, 'width' => 'col-md-12', 'placeholder' => 'SEO description', 'id' => null]]) ?>
+                <div class="last-row">
+                    <h5 class="mt-5 ">Altri strumenti</h5>
+                    <div class="row border badge-light mt-4 mx-1 p-3">
+                        <?= view('Lc5\Cms\Views\form-cmp/text', ['item' => ['label' => 'SEO title', 'name' => 'seo_title', 'value' => $entity->seo_title, 'width' => 'col-md-12', 'placeholder' => 'seo title', 'id' => null]]) ?>
+                        <?= view('Lc5\Cms\Views\form-cmp/text-area', ['item' => ['label' => 'SEO description', 'name' => 'seo_description', 'value' => $entity->seo_description, 'width' => 'col-md-12', 'placeholder' => 'SEO description', 'id' => null]]) ?>
+                    </div>
                 </div>
             <?php } ?>
-
-
         </div>
-        <div class="col-12 col-lg-3 mt-4 pl-lg-5 text-dark scheda-sb">
+        <div class="scheda-sb margin-top-0">
             <div class="row">
                 <div class="col-12">
                     <div class="bg-light p-4 px-lg-2 py-lg-3 rounded">
@@ -59,8 +81,6 @@
                                 <?= view('Lc5\Cms\Views\form-cmp/img-single', ['item' => ['label' => 'Copertina', 'name' => 'main_img_id', 'value' => $entity->main_img_id, 'width' => 'col-12', 'src' => $entity->main_img_thumb]]) ?>
                             </div>
                         </div>
-
-
                     </div>
                 </div>
             </div>
