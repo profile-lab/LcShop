aeada787c0799fa3ece1c3d81cc6566aea4e6a7d
profile-lab <info@profile-lab.it>
2023-11-15 19:44:53 +0100

    rimossi install inutili

--- Cms/Routes/lcshop-install.php
+++ Cms/Routes/lcshop-install.php
@@ -1,4 +1,1 @@
 <?php
-$routes->match(['get', 'post'], 'lc-admin/first-login', '\Lc5\Cms\Controllers\Admins::firstLogin', ['as' => 'lc_first_login']); //, ['filter' => 'noauth']
-$routes->match(['get'], 'lc-admin/update-db', '\Lc5\Cms\Controllers\Migrate::update', ['as' => 'lc_update_db']); //, ['filter' => 'noauth']
-// $routes->match(['get'], 'lc-admin/datibase-db', '\Lc5\Cms\Controllers\Migrate::datiBase', ['as' => 'lc_datiBase_db']); //, ['filter' => 'noauth']
