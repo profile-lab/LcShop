35a28dfe86f9ffca6f56188f7ae445c1e9c6b32f
profile-lab <info@profile-lab.it>
2023-11-27 17:05:35 +0100

    Tools calcoli iva

--- Cms/Views/products/scheda.php
+++ Cms/Views/products/scheda.php
@@ -181,6 +181,8 @@
                         </div>
 
 
+
+
                     </div>
                 </div>
             </div>
@@ -270,5 +272,242 @@
 <script type="text/html" id="custom_field_item_code-prodotti" style="display: none;">
     <?= view('Lc5\Cms\Views\part-cmp/custom-field-item', ['item' => ['keys_source' => $custom_fields_keys_prodotti]]) ?>
 </script>
+
+<script type="text/html" id="calcola-imponibile-modal" style="display: none;">
+    <div class="modal fade" id="input-tools-modal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+
+        <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable modal-xl" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title">Utility scheda</h5>
+                    <button type="button" class="btn-close close_modal" data-bs-dismiss="modal" aria-label="Close"><span class="oi oi-x"></span></button>
+                </div>
+                <div class="modal-body">
+                    <div class="input-tools-cnt">
+                        <div class="input-tool-item">
+                            <label>Scorpora Iva</label>
+                            <div class="input-tools-row input-tools-from">
+                                <input type="number" step="0.01" data-decimal="2" class="form-control scorpora_iva_from" placeholder="Prezzo Lordo">
+                                <button type="button" class="user_tool user_tool-scorpora"><span class="oi oi-calculator"></span></button>
+                            </div>
+                            <div class="input-tools-row input-tools-result">
+                                <div class="input-tools-result-label">Imponibile </div>
+                                <input type="number" readonly="" class="form-control  readonly_enabled disabled scorpora_iva_result" placeholder="">
+                            </div>
+                        </div>
+
+                        <div class="input-tool-item">
+                            <label>Calcola ivato</label>
+                            <div class="input-tools-row input-tools-from">
+                                <input type="number" step="0.01" data-decimal="2" class="form-control calcola_ivato_from" placeholder="Prezzo Imponibile">
+                                <button type="button" class="user_tool user_tool-calcola_ivato"><span class="oi oi-calculator"></span></button>
+                            </div>
+                            <div class="input-tools-row input-tools-result">
+                                <div class="input-tools-result-label">Lordo </div>
+                                <input type="number" readonly="" class="form-control  readonly_enabled disabled calcola_ivato_result" placeholder="">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</script>
+
+
+
+
+<script language="javascript">
+    //----------------------------------------------
+    //----------------------------------------------
+
+    var utilityIVA = {};
+
+    var formatReturn = function(imponibile, aliquotaIVA, importoIVA, totale, retValNoCallback, callback) {
+        var retObj = {
+            imponibile: Number(imponibile),
+            aliquotaIVA: Number(aliquotaIVA),
+            importoIVA: Number(importoIVA),
+            totale: Number(totale)
+        };
+        if (callback) {
+            callback(null, retObj); //callback in format 'callback(err, data)'
+        } else {
+            return retValNoCallback;
+        }
+    };
+
+    utilityIVA.scorporoIVA = function(totale, aliquotaIVA, callback) {
+        return this.calcolaImponibile(totale, aliquotaIVA, callback);
+    };
+    utilityIVA.calcolaImponibile = function(totale, aliquotaIVA, callback) {
+        if (aliquotaIVA == -100) {
+            if (callback) {
+                return callback(true, 'Error: division by zero');
+            } else {
+                return 'Error: division by zero';
+            }
+        }
+        var imponibile = totale / ((100 + aliquotaIVA) / 100);
+        var importoIVA = totale - imponibile;
+        return formatReturn(imponibile, aliquotaIVA, importoIVA, totale, imponibile, callback);
+    };
+
+    utilityIVA.calcolaImportoIvato = function(imponibile, aliquotaIVA, callback) {
+        var importoIVA = (imponibile * aliquotaIVA) / 100;
+        var totale = Number(imponibile) + Number(importoIVA);
+        return formatReturn(imponibile, aliquotaIVA, importoIVA, totale, totale, callback);
+    };
+
+    utilityIVA.calcolaAliquotaIVA = function(imponibile, totale, callback) {
+        var importoIVA = totale - imponibile;
+        if (imponibile == 0 || importoIVA == 0) {
+            return formatReturn(imponibile, 0, importoIVA, totale, 0, callback);
+        }
+        var aliquotaIVA = /* Math.round( */ 100 / (imponibile / importoIVA) /* ) */ ;
+        return formatReturn(imponibile, aliquotaIVA, importoIVA, totale, aliquotaIVA, callback);
+    };
+
+    utilityIVA.calcolaImponibileDaIVA = function(importoIVA, aliquotaIVA, callback) {
+        if (aliquotaIVA == 0) {
+            return formatReturn(0, aliquotaIVA, importoIVA, 0, 0, callback);
+        }
+        var imponibile = (importoIVA / aliquotaIVA) * 100;
+        var totale = imponibile + importoIVA;
+        return formatReturn(imponibile, aliquotaIVA, importoIVA, totale, imponibile, callback);
+    };
+
+    utilityIVA.calcolaTotaleDaIVA = function(importoIVA, aliquotaIVA, callback) {
+        if (aliquotaIVA == 0) {
+            return formatReturn(0, aliquotaIVA, importoIVA, 0, 0, callback);
+        }
+        var imponibile = (importoIVA / aliquotaIVA) * 100;
+        var totale = imponibile + importoIVA;
+        return formatReturn(imponibile, aliquotaIVA, importoIVA, totale, totale, callback);
+    };
+
+    //----------------------------------------------
+    //----------------------------------------------
+    //----------------------------------------------
+
+    $(document).ready(function() {
+        // 
+        $('.form-field-price label').append(' <a href="#" meta-rel="calcola-imponibile-modal" class="openToolBtn"><i>Calcola</></a>');
+
+        $('body').on('click', '.openToolBtn', function(e) {
+            e.preventDefault();
+            const modalToOpenTrgt = $(this).attr('meta-rel');
+            var modal_html = $('#' + modalToOpenTrgt).text();
+            let modal_obj = $(modal_html);
+            $('body').append(modal_obj);
+            // 
+            $('.user_tool-scorpora').click(calcolaScorporoDaInput);
+            $('.scorpora_iva_from').keypress(calcolaScorporoDaInputInvio);
+            // 
+            $('.user_tool-calcola_ivato').click(calcolaIvatoDaInput);
+            $('.calcola_ivato_from').keypress(calcolaIvatoDaInputInvio);
+        });
+
+
+    });
+
+    //----------------------------------------------
+    //-- SCORPORA IVA
+    //----------------------------------------------
+
+    function calcolaScorporoDaInputInvio(e) {
+        if (e.which == 13) {
+            calcolaScorporoDaInput(e);
+        }
+    }
+
+    function calcolaScorporoDaInput(e) {
+        e.preventDefault();
+        var scorpora_iva_from = $('.scorpora_iva_from').val();
+        var scorpora_iva_result = $('.scorpora_iva_result');
+        if (scorpora_iva_from) {
+            utilityIVA.scorporoIVA(scorpora_iva_from, 22, function(err, data) {
+                const cImponibile = data.imponibile;
+                if (cImponibile) {
+                    scorpora_iva_result.val(cImponibile.toFixed(2));
+                }
+            });
+        }
+        return false;
+    }
+
+    //----------------------------------------------
+    //-- CALCOLA IVATO
+    //----------------------------------------------
+
+    function calcolaIvatoDaInputInvio(e) {
+        if (e.which == 13) {
+            calcolaIvatoDaInput(e);
+        }
+    }
+
+    function calcolaIvatoDaInput(e) {
+        e.preventDefault();
+        var calcola_ivato_from = $('.calcola_ivato_from').val();
+        var calcola_ivato_result = $('.calcola_ivato_result');
+        if (calcola_ivato_from) {
+            utilityIVA.calcolaImportoIvato(calcola_ivato_from, 22, function(err, data) {
+                const cIvato = data.totale;
+                if (cIvato) {
+                    calcola_ivato_result.val(cIvato.toFixed(2));
+                }
+            });
+        }
+        return false;
+    }
+</script>
+<style>
+    .input-tools-cnt {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: space-around;
+        padding: 1em;
+    }
+
+    .input-tool-item {
+        padding: .8em .5em;
+        margin: .8em .5em;
+        background-color: #f8f9fa;
+    }
+
+    .input-tools-row {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: space-between;
+        padding: .3em .2em;
+    }
+    .input-tools-row input{
+        width: auto
+    }
+
+    .input-tools-from {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: space-between;
+    }
+
+    .input-tools-result{
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: space-between;
+    
+    }
+    .input-tools-result-label{
+        font-size: .7em;
+        font-weight: bold;
+        margin-right: .5em;
+    }
+
+</style>
+
 
 <?= $this->endSection() ?>
